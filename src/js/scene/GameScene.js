3198296e0ede38cad7cfe05dcd7edc415de660f4
Foysal Ahamed <thisisfoysal@gmail.com>
2018-09-11 16:50:06 +0600

    Added game title

--- src/js/scene/GameScene.js
+++ src/js/scene/GameScene.js
@@ -3,7 +3,15 @@ class GameScene extends Phaser.Scene {
         super(T3.GameOptions.scenes.gameScene);
     }
 
+    init() {
+        this.allowUserInput = false;
+        
+        this.gameTitle = null;
+    }
+
     create() {
-        console.log('GameScene created.');
+        /* Adding UI images */
+        this.gameTitle = this.add.image(T3.game.config.width / 2, 90, "gametitle");
+        this.gameTitle.setOrigin(0.5, 0);
     }
 }
