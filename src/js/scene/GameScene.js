63ed227d6a9b5b1f756a184f0eb39def98875459
Foysal Ahamed <thisisfoysal@gmail.com>
2018-09-11 17:25:56 +0600

    Showing board draw animation after loading main game scene.

--- src/js/scene/GameScene.js
+++ src/js/scene/GameScene.js
@@ -17,7 +17,7 @@ class GameScene extends Phaser.Scene {
         this.gameTitle.setOrigin(0.5, 0);
 
         /* Game board */
-        this.board = this.add.sprite(T3.game.config.width / 2, T3.game.config.height / 2, "board", 12);
+        this.board = this.add.sprite(T3.game.config.width / 2, T3.game.config.height / 2, "board", 0);
         this.board.setOrigin(0.5, 0.5);
 
         this.restart = this.add.image(T3.game.config.width / 2, T3.game.config.height - 90, "restart");
@@ -26,5 +26,39 @@ class GameScene extends Phaser.Scene {
         this.restart.on('pointerdown', function () {
             this.scene.start(T3.GameOptions.scenes.mainMenuScene);
         }, this);
+
+        this.showEntryAnimation();
+    }
+
+    showEntryAnimation() {
+        let timerConfig = {
+            delay: T3.GameOptions.animations.iconAppearAnimationDelay,
+            callback: function () {
+
+                if (!this.anims.get('drawBoardAnim')) {
+                    let boardAnimConfig = {
+                        key: 'drawBoardAnim',
+                        frames: this.anims.generateFrameNumbers('board', {
+                            start: 0,
+                            end: 12
+                        }),
+                        frameRate: 25,
+                    };
+                    this.anims.create(boardAnimConfig);
+                }
+
+                this.board.on('animationcomplete', function (animation) {
+                    if (animation.key === 'drawBoardAnim') {
+                        console.log('Board animation complete');
+                    }
+                }, this);
+
+                this.board.anims.play('drawBoardAnim');
+            },
+            callbackScope: this,
+            paused: false
+        };
+
+        this.time.addEvent(timerConfig);
     }
 }
