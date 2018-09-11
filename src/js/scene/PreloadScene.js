a2211233621cefb48cf2737a2c7d8e6b3a4dc104
Foysal Ahamed <thisisfoysal@gmail.com>
2018-09-11 12:18:32 +0600

    Added game loading bar

--- src/js/scene/PreloadScene.js
+++ src/js/scene/PreloadScene.js
@@ -11,9 +11,76 @@ class PreloadScene extends Phaser.Scene {
         /* Showing application icon */
         this.appIcon = this.add.image(T3.game.config.width / 2, T3.game.config.height / 4 - 100, "logo");
         this.appIcon.setOrigin(0.5, 0);
+
+        /* Adding and configuring progress bar */
+        this.setupProgressBar();
     }
 
     create() {
         console.log("Created PreloadScene");
+    }
+
+    setupProgressBar() {
+        let progressBar = this.add.graphics();
+        let progressBox = this.add.graphics();
+        progressBox.fillStyle(0x3a3a3c, 0.8);
+        progressBox.fillRect(T3.game.config.width / 4, T3.game.config.height - (T3.game.config.height / 4), T3.game.config.width / 2, 50);
+
+        let width = this.cameras.main.width / 2;
+        let height = (this.cameras.main.height / 4) * 3;
+
+        /* Initializing text for progress status */
+        let loadingText = this.make.text({
+            x: width,
+            y: height - 30,
+            text: 'Loading...',
+            style: {
+                font: '34px monospace',
+                fill: '#16a085'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        let percentText = this.make.text({
+            x: width,
+            y: height + 25,
+            text: '0%',
+            style: {
+                font: '30px monospace',
+                fill: '#16a085'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        let assetText = this.make.text({
+            x: width,
+            y: height + 80,
+            text: '',
+            style: {
+                font: '30px monospace',
+                fill: '#16a085'
+            }
+        });
+        assetText.setOrigin(0.5, 0.5);
+
+        /* Load modules callback */
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0x3a3a3c, 1);
+            progressBar.fillRect(T3.game.config.width / 4 + 20, T3.game.config.height - (T3.game.config.height / 4) + 10, 300 * value, 30);
+        });
+
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Loading asset: ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
     }
 }
