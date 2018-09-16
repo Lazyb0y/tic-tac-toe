84ff200a10907d6a6bda1882b00626a72cd48a03
Lazyb0y <thisisfoysal@gmail.com>
2018-09-17 00:22:58 +0600

    Added animation while showing game result

--- src/js/scene/GameEndScene.js
+++ src/js/scene/GameEndScene.js
@@ -20,13 +20,11 @@ class GameEndScene extends Phaser.Scene {
         let gameTitle = this.add.image(T3.game.config.width / 2, 90, "gametitle");
         gameTitle.setOrigin(0.5, 0);
 
-        this.resultText = this.add.image(T3.game.config.width / 2, (T3.game.config.height / 4) * 1.5, "drawText");
+        this.resultText = this.add.image(T3.game.config.width / 2, T3.game.config.height + 90, "drawText");
         this.resultText.setOrigin(0.5, 0);
-        this.resultText.alpha = 0;
 
-        this.winnerSymbol = this.add.image(T3.game.config.width / 2, T3.game.config.height / 2, "egg");
+        this.winnerSymbol = this.add.image(T3.game.config.width / 2, T3.game.config.height + 512, "egg");
         this.winnerSymbol.setOrigin(0.5, 0);
-        this.winnerSymbol.alpha = 0;
 
         this.playAgain = this.add.image(T3.game.config.width / 2, T3.game.config.height + 90, "playAgain");
         this.playAgain.setOrigin(0.5, 0);
@@ -60,29 +58,37 @@ class GameEndScene extends Phaser.Scene {
     }
 
     showResult() {
+        if (this.winner === PlayerType.Human) {
+            this.resultText.setTexture('winText');
+            this.winnerSymbol.setTexture('cup');
+        }
+        else if (this.winner === PlayerType.Bot) {
+            this.resultText.setTexture('looseText');
+            this.winnerSymbol.setTexture('robotFace');
+        }
+        else {
+            this.resultText.setTexture('drawText');
+            this.winnerSymbol.setTexture('egg');
+        }
+
         this.tweens.add({
-            targets: [this.playAgain],
-            y: T3.game.config.height - 180,
+            targets: [this.resultText],
+            y: (T3.game.config.height / 4) * 1.5,
             duration: T3.GameOptions.animations.buttonTweenDelay,
             callbackScope: this,
             onComplete: function () {
-                if (this.winner === PlayerType.Human) {
-                    this.resultText.setTexture('winText');
-                    this.winnerSymbol.setTexture('cup');
-                }
-                else if (this.winner === PlayerType.Bot) {
-                    this.resultText.setTexture('looseText');
-                    this.winnerSymbol.setTexture('robotFace');
-                }
-                else {
-                    this.resultText.setTexture('drawText');
-                    this.winnerSymbol.setTexture('egg');
-                }
-
                 this.tweens.add({
-                    targets: [this.resultText, this.winnerSymbol],
-                    alpha: 1,
-                    duration: T3.GameOptions.animations.alphaTweenSpeed
+                    targets: [this.winnerSymbol],
+                    y: T3.game.config.height / 2,
+                    duration: T3.GameOptions.animations.buttonTweenDelay * 2,
+                    callbackScope: this,
+                    onComplete: function () {
+                        this.tweens.add({
+                            targets: [this.playAgain],
+                            y: T3.game.config.height - 180,
+                            duration: T3.GameOptions.animations.buttonTweenDelay
+                        });
+                    }
                 });
             }
         });
